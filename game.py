c40f2bb3e04d6b13d5f686f47c77ad9c51a2e64d
veromar.c96@gmail.com <veronicamartinez@Veronicas-MacBook-Air.local>
2025-11-13 18:53:02 -0500

    Updated files

--- game.py
+++ game.py
@@ -1,10 +1,42 @@
-def nivel_basico():
+def levelI():
     with open('map1.txt','r+') as map:
         structure = map.readlines()
+        new_map = []
         for i in structure:
-            #structure.append(int(x) for x in i.strip('\n'))
-            # print(map.readlines())
-            # print('\n')
             print(i)
+            for j in i:
+                new_map.append(i).append(j)
+    return new_map
 
-nivel_basico()
+def levelII():
+    with open('map2.txt','r+') as map:
+        structure = map.readlines()
+        new_map = []
+        for i in structure:
+            print(i)
+            new_map.append(i)
+    return new_map
+
+
+def move_cursor(map,dir):
+    new_map = map
+    print(new_map)
+    print(type(new_map))
+    i = 0
+    j = 0
+    for line in map:
+        print(line)
+        i += 1
+        for pos in line:
+            j += 1
+            print(new_map.size())
+            if dir == "A":
+                if new_map[line.index()][(pos.index())-1] == "|":
+                    print("error")
+                pass
+
+levelI()
+
+dir = 'A'
+
+move_cursor(levelI(),dir)
